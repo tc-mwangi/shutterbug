f17af6fbd86022e1321327419c0b2f72e0b1e1b5
tc-mwangi <wanjirumwangi.learn@gmail.com>
2019-02-28 16:48:51 +0300

    create 3 models

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    '''creates instances of images
+    
+    Arguments:
+        models {[type]} -- [description]
+    '''
+
+
+
+
+class Location(models.Model):
+    '''creates instances of locations
+    
+    Arguments:
+        models {[type]} -- [description]
+    '''
+
+
+
+
+
+class Category(models.Model):
+    '''creates instances of category
+    
+    Arguments:
+        models {[type]} -- [description]
+    '''
