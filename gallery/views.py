3e1d404b94e06430fc798bbd8481ea550ebe3a91
tc-mwangi <wanjirumwangi.learn@gmail.com>
2019-03-01 10:18:35 +0300

    define required templates

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+import datetime as datetime
+from .models import Image, Location, Category
 # Create your views here.
+
+
+def home(request):
+    '''renders home page
+    
+    Arguments:
+        request {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    '''
+    return render(request, 'main/home.html')
+
+
+def gallery(request):
+    '''renders gallery page
+    
+    Arguments:
+        request {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    '''
+
+    return render(request, 'main/gallery.html')
+
+
+def search(request):
+    '''renders search results
+    
+    Arguments:
+        request {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    '''
+
+    return render(request, 'main/search.html')
